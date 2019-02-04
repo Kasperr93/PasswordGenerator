6c7345ac0eb1e0393a179154b89ea7c41f5eb550
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-04 15:22:04 +0100

    - Add window center.

--- src/kasperek/gui/MainWindow.java
+++ src/kasperek/gui/MainWindow.java
@@ -1,6 +1,7 @@
 package kasperek.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow {
     private JFrame frame;
@@ -15,7 +16,21 @@ public class MainWindow {
         frame.setSize(400, 600);
         frame.setResizable(false);
         frame.setContentPane(panel);
+
+        windowCenter();
+        initialization();
+
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void windowCenter() {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
+    }
+
+    private void initialization() {
     }
 }
