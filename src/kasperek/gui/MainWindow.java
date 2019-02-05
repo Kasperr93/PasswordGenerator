032422441d6c43ff02b79ba8696672e79d7592f5
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-05 15:17:13 +0100

    - Add title label method; - Add text field for generated password;
    - Change some values to final variables;
    - Change some methods description;
    - Change positions some elements in GUI;
    
    - Small refactor to better reading of the code;

--- src/kasperek/gui/MainWindow.java
+++ src/kasperek/gui/MainWindow.java
@@ -13,6 +13,12 @@ public class MainWindow {
     private JFrame frame;
     private JPanel panel;
 
+    private final int ELEMENT_HEIGHT = 20;
+    private final int ELEMENT_WIDTH = 430;
+    private final int ELEMENT_X_POSITION = 100;
+
+    private JLabel titleLabel;
+
     private JCheckBox numbersCheckBox;
     private JCheckBox specialCharactersCheckBox;
 
@@ -22,6 +28,7 @@ public class MainWindow {
     private ButtonGroup radioButtonLettersGroup;
 
     private JSlider passwordLengthSlider;
+    private JTextField generatedPasswordTextField;
 
     /**
      * Default constructor. It is responsibility for setter all needed parameters for window. Also it is responsibility
@@ -49,7 +56,7 @@ public class MainWindow {
     }
 
     /**
-     * The method is responsibility for auto centering the window.
+     * The method is responsibility for the auto centering the window.
      */
 
     private void windowCenter() {
@@ -64,22 +71,38 @@ public class MainWindow {
      */
 
     private void initialization() {
+        initializeTitleLabel();
         initializeNumbersCheckBox();
         initializeSpecialCharactersCheckBox();
         initializeSmallLettersRadioButton();
         initializeBigLettersRadioButton();
         initializeSmallAndBigLettersRadioButton();
         initializePasswordLengthSlider();
+        initializeGeneratedPasswordTextField();
     }
 
     /**
-     * The method is responsibility for creating checkbox to choose to create the password with numbers.
+     * The method shows the title label.
+     */
+
+    private void initializeTitleLabel() {
+        titleLabel = new JLabel("Make your strong password!");
+        titleLabel.setBounds(45, 30, ELEMENT_WIDTH, 40);
+        titleLabel.setForeground(Color.LIGHT_GRAY);
+        titleLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 24));
+        titleLabel.setVisible(true);
+
+        panel.add(titleLabel);
+    }
+
+    /**
+     * The method is responsibility for creating the checkbox to choose to create the password with numbers.
      */
 
     private void initializeNumbersCheckBox() {
         numbersCheckBox = new JCheckBox("Use numbers");
 
-        numbersCheckBox.setBounds(80, 125, 430, 20);
+        numbersCheckBox.setBounds(ELEMENT_X_POSITION, 100, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         numbersCheckBox.setForeground(Color.LIGHT_GRAY);
         numbersCheckBox.setVisible(true);
 
@@ -87,13 +110,13 @@ public class MainWindow {
     }
 
     /**
-     * The method is responsibility for creating checkbox to choose to create the password with special characters.
+     * The method is responsibility for creating the checkbox to choose to create the password with special characters.
      */
 
     private void initializeSpecialCharactersCheckBox() {
         specialCharactersCheckBox = new JCheckBox("Use special characters");
 
-        specialCharactersCheckBox.setBounds(80, 150, 430, 20);
+        specialCharactersCheckBox.setBounds(ELEMENT_X_POSITION, 125, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         specialCharactersCheckBox.setForeground(Color.LIGHT_GRAY);
         specialCharactersCheckBox.setVisible(true);
 
@@ -101,13 +124,13 @@ public class MainWindow {
     }
 
     /**
-     * The method is responsibility for creating radio button to choose to create the password with only small letters.
+     * The method is responsibility for creating the radio button to choose to create the password with only small letters.
      */
 
     private void initializeSmallLettersRadioButton() {
         smallLettersRadioButton = new JRadioButton("Use only small letters");
 
-        smallLettersRadioButton.setBounds(80, 175, 430, 20);
+        smallLettersRadioButton.setBounds(ELEMENT_X_POSITION, 150, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         smallLettersRadioButton.setForeground(Color.LIGHT_GRAY);
         smallLettersRadioButton.setSelected(true);
         smallLettersRadioButton.setVisible(true);
@@ -117,13 +140,13 @@ public class MainWindow {
     }
 
     /**
-     * The method is responsibility for creating radio button to choose to create the password with only big letters.
+     * The method is responsibility for creating the radio button to choose to create the password with only big letters.
      */
 
     private void initializeBigLettersRadioButton() {
         bigLettersRadioButton = new JRadioButton("Use only big letters");
 
-        bigLettersRadioButton.setBounds(80, 200, 430, 20);
+        bigLettersRadioButton.setBounds(ELEMENT_X_POSITION, 175, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         bigLettersRadioButton.setForeground(Color.LIGHT_GRAY);
         bigLettersRadioButton.setVisible(true);
 
@@ -132,14 +155,14 @@ public class MainWindow {
     }
 
     /**
-     * The method is responsibility for creating radio button to choose to create the password with small
+     * The method is responsibility for creating the radio button to choose to create the password with small
      * and big letters.
      */
 
     private void initializeSmallAndBigLettersRadioButton() {
         smallAndBigLettersRadioButton = new JRadioButton("Use small and big letters");
 
-        smallAndBigLettersRadioButton.setBounds(80, 225, 430, 20);
+        smallAndBigLettersRadioButton.setBounds(ELEMENT_X_POSITION, 200, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         smallAndBigLettersRadioButton.setForeground(Color.LIGHT_GRAY);
         smallAndBigLettersRadioButton.setVisible(true);
 
@@ -156,26 +179,39 @@ public class MainWindow {
         JLabel passwordLengthLabel = new JLabel("Password length");
         JLabel lengthLabel = new JLabel(String.valueOf(passwordLengthSlider.getValue()));
 
-        passwordLengthSlider.setBounds(10, 280, 360, 20);
+        passwordLengthSlider.setBounds(10, 285, 360, ELEMENT_HEIGHT);
         passwordLengthSlider.setPaintTicks(true);
         passwordLengthSlider.setPaintLabels(true);
         passwordLengthSlider.setMinorTickSpacing(8);
         passwordLengthSlider.addChangeListener(e -> lengthLabel.setText(String.valueOf(passwordLengthSlider.getValue())));
-
         passwordLengthSlider.setVisible(true);
-        panel.add(passwordLengthSlider);
 
-        passwordLengthLabel.setBounds(10, 255, 430, 20);
+        passwordLengthLabel.setBounds(20, 255, ELEMENT_WIDTH, ELEMENT_HEIGHT);
         passwordLengthLabel.setForeground(Color.LIGHT_GRAY);
-
         passwordLengthLabel.setVisible(true);
-        panel.add(passwordLengthLabel);
 
-
-        lengthLabel.setBounds(375, 275, 20, 20);
+        lengthLabel.setBounds(370, 280, 20, ELEMENT_HEIGHT);
         lengthLabel.setForeground(Color.LIGHT_GRAY);
-
         lengthLabel.setVisible(true);
+
+        panel.add(passwordLengthSlider);
+        panel.add(passwordLengthLabel);
         panel.add(lengthLabel);
+    }
+
+    /**
+     * The method shows the text field with the generated password.
+     */
+
+    private void initializeGeneratedPasswordTextField() {
+        generatedPasswordTextField = new JTextField();
+
+        generatedPasswordTextField.setBounds(10, 360, 380, ELEMENT_HEIGHT);
+        generatedPasswordTextField.setBackground(Color.LIGHT_GRAY);
+        generatedPasswordTextField.setForeground(Color.WHITE);
+        generatedPasswordTextField.setEditable(false);
+        generatedPasswordTextField.setVisible(true);
+
+        panel.add(generatedPasswordTextField);
     }
 }
