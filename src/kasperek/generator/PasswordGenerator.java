01c88c539e6a8147177dc9dc70909062648f95e0
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-07 17:40:20 +0100

    - Add generate password method; - Add documentation.

--- src/kasperek/generator/PasswordGenerator.java
+++ src/kasperek/generator/PasswordGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 /**
  * @author Tomasz Kasperek
- * @version 0.2 02/05/2019
+ * @version 0.3 02/07/2019
  * @see kasperek.gui.MainWindow
  * @since 0.1
  */
@@ -28,13 +28,67 @@ public class PasswordGenerator {
         numbers = "0123456789";
     }
 
+    /**
+     * The method generating the password with user preferences.
+     *
+     * @param useNumbers           the password will contain numbers.
+     * @param useSpecialCharacters the password will contain special characters.
+     * @param useSmallLetters      the password will only have lowercase letters.
+     * @param useBigLetters        the password will only have uppercase letters.
+     * @param length               the password length (can be from 8 to 64 characters).
+     * @return the generated password.
+     */
+
+    public String generatedPassword(boolean useNumbers, boolean useSpecialCharacters, boolean useSmallLetters, boolean useBigLetters, int length) {
+        StringBuilder generatedPassword = new StringBuilder();
+        int number;
+
+        for (int i = length; i < 0; i--) {
+            number = random.nextInt(100);
+
+            if (number >= 0 && number < 25 && useNumbers) {
+                generatedPassword.append(generateNumber());
+            } else if (number >= 25 && number < 40 && useSpecialCharacters) {
+                generatedPassword.append(generateSpecialCharacter());
+            } else {
+                generatedPassword.append(generateLetter());
+            }
+        }
+
+        if (useSmallLetters) {
+            return generatedPassword.toString().toLowerCase();
+        } else if (useBigLetters) {
+            return generatedPassword.toString().toUpperCase();
+        } else {
+            return generatedPassword.toString();
+        }
+    }
+
+    /**
+     * The method draw lots a single character (can be from a-z to A-Z characters).
+     *
+     * @return the random character.
+     */
+
     private char generateLetter() {
         return letters.charAt(random.nextInt(letters.length()));
     }
 
+    /**
+     * The method draw lots a single number (can be from 0 to 9 characters).
+     *
+     * @return the random number.
+     */
+
     private char generateNumber() {
         return numbers.charAt(random.nextInt(numbers.length()));
     }
+
+    /**
+     * The method draw lots a single special character.
+     *
+     * @return the random special character.
+     */
 
     private char generateSpecialCharacter() {
         return specialCharacters.charAt(random.nextInt(specialCharacters.length()));
