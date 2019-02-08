9b5152f95e9d591099e5247fd6d749b9cc5c2afb
Tomasz Kasperek <kasperek1993@gmail.com>
2019-02-08 10:54:26 +0100

    - Implementation the main method.

--- src/kasperek/main/Main.java
+++ src/kasperek/main/Main.java
@@ -1,8 +1,18 @@
 package kasperek.main;
 
-public class Main {
+import kasperek.gui.MainWindow;
 
+/**
+ * @author Tomasz Kasperek
+ * @version 1.0 02/05/2019
+ * @see kasperek.generator.PasswordGenerator
+ * @see MainWindow
+ * @since 0.1
+ */
+
+public class Main {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
-	// write your code here
+        var window = new MainWindow();
     }
 }
